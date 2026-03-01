fb7d45c9880b1d0d9bb35e42f16911d018c32328
Jose Diogo <jose.diogo100407@gmail.com>
2026-03-01 19:37:35 -0300

    Aula 21 do curso de C#

---
+++ AN - Advanced Business Development with .NET/Curso C#/aula21/TestProject/Program.cs
@@ -0,0 +1,51 @@
+﻿// Parte 1 - Aprendendo Bits
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+
+
+// Parte 2 - Pontos flutuantes
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+
+// Parte 3 - Referencias
+int[] data;
+data = new int[3];
+// Ou
+int[] data2 = new int[3];
+
+
+// Exemplo de variavel normal
+int val_A = 2;
+int val_B = val_A;
+val_B = 5;
+Console.WriteLine("--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
+
+// Exemplo com Referencia (Matriz)
+int[] ref_A= new int[1];
+ref_A[0] = 2;
+int[] ref_B = ref_A;
+ref_B[0] = 5;
+Console.WriteLine("--Reference Types--");
+Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+// no terminal o ref_A vai ser o mesmo valor de ref_B, por conta que a informação do ref_A foi alocada no int[1] aonde o ref_B pega a informação tambem
+
