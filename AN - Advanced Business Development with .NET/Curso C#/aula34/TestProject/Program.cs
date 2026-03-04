afe5a9a97a72b999d81659a9b6d6e350d150d38d
Jose Diogo <jose.diogo100407@gmail.com>
2026-03-04 19:37:19 -0300

    Aula 34 do curso de C#

---
+++ AN - Advanced Business Development with .NET/Curso C#/aula34/TestProject/Program.cs
@@ -0,0 +1,29 @@
+﻿static void LoadProducts(string[,] products, int productCount)
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < productCount; i++)
+    {
+        int num1 = rand.Next(1, 10000) + 10000;
+        int num2 = rand.Next(1, 101);
+
+        string prodID = num1.ToString();
+
+        if (num2 < 91)
+        {
+            products[i, 1] = "existing";
+        }
+        else if (num2 == 91)
+        {
+            products[i, 1] = "new";
+            prodID = prodID + "-n";
+        }
+        else
+        {
+            products[i, 1] = "obsolete";
+            prodID = prodID + "-0";
+        }
+
+        products[i, 0] = prodID;
+    }
+}
