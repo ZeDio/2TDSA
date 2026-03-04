96373e4c517bb3ebb11ae5fbc51e9c65399df0c5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-04 09:34:48 -0300

    aula 6 MA - 04/03/2026

---
+++ MA - Mobile Application Development/aula 6/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fristName;
+fristName = "Lucas";
+fristName = 10;
+console.log(fristName);
+//# sourceMappingURL=index.js.map
---
+++ MA - Mobile Application Development/aula 6/index.ts
@@ -0,0 +1,1 @@
+let variavel: string | number = "leonardo";
