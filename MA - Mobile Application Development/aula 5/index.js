8499f54d6bfe94c67d14102f93bd93cfac679d87
Jose Diogo <jose.diogo100407@gmail.com>
2026-03-04 19:30:35 -0300

    Aula 33 do curso de C#

--- MA - Mobile Application Development/aula 5/index.js
+++ MA - Mobile Application Development/aula 5/index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fristName;
+fristName = "Lucas";
+fristName = 10;
+console.log(fristName);
+//# sourceMappingURL=index.js.map
--- MA - Mobile Application Development/aula 6/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let fristName;
-fristName = "Lucas";
-fristName = 10;
-console.log(fristName);
-//# sourceMappingURL=index.js.map
