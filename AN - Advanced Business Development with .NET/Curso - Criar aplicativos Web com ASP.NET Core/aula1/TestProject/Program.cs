9424c162229956c2ea568902345e936c8c267713
Jose Diogo <jose.diogo100407@gmail.com>
2026-03-16 10:38:57 -0300

    Aula 1 do Curso de C# 'Criar aplicativos Web com ASP.NET Core'

---
+++ AN - Advanced Business Development with .NET/Curso - Criar aplicativos Web com ASP.NET Core/aula1/TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿// Console.WriteLine("Hello World!");
+
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+
+// Desafio
+
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
